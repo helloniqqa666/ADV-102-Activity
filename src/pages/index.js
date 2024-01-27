0f861e012bc697786177995014065c206b53962a
JRSilverio <johnrey.silverio@hcdc.edu.ph>
2024-01-27 15:25:54 +0800

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+
+import TodoList from '../components/TodoList';
+
+const Home = () => {
+  return (
+    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <TodoList />
+    </div>
+  );
+};
+
+export default Home;
